b828a1f0ef6dea081ff4a0ee099455b21bb17f96
Ramakant Kumar <ramakantnmiet123@gmail.com>
2022-11-13 17:48:05 +0530

    CustomizedSet is Completed by Ramakant kumar

--- JustTest1/src/com/delhi/CustomizeSet.java
+++ JustTest1/src/com/delhi/CustomizeSet.java
@@ -1,5 +1,53 @@
 package com.delhi;
 
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+class Student{
+	int sid;
+	String sname;
+	Student(int sid,String sname){
+		this.sid=sid;
+		this.sname=sname;
+	}
+	public String toString() {
+		return sid+"--"+sname;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(sid, sname);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		return sid == other.sid && Objects.equals(sname, other.sname);
+	}
+	
+}
 public class CustomizeSet {
+	public static void main(String[] args) {
+		Student std1=new Student(101, "Ramakant");
+		Student std2=new Student(102, "Shyamakant");
+		Student std3=new Student(101, "Ramakant");
+		Student std4=new Student(102, "Shyamakant");
+		Student std5=new Student(101, "Ramakant");
+		Student std6=new Student(102, "Shyamakant");
+		Set<Student> hs=new HashSet<Student>();
+		hs.add(std1);
+		hs.add(std2);
+		hs.add(std3);
+		hs.add(std4);
+		hs.add(std5);
+		hs.add(std6);
+		System.out.println(hs);
+		
+	}
 
 }
